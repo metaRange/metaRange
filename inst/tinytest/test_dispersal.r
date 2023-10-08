9a0be08a80f9d98bcb1d922abdb250380e9f16ef
srfall <65958585+srfall@users.noreply.github.com>
2023-10-08 13:43:12 +0200

    more test for dispersal

--- inst/tinytest/test_dispersal.r
+++ inst/tinytest/test_dispersal.r
@@ -35,4 +35,43 @@ expect_equal(
     info = "dispersal with a suitability of 1 gives the same result as when calling the function without it."
 )
 
+abu <- matrix(0, nrow = n, ncol = n)
+abu[n / 2, n / 2] <- 9
+kernel <- matrix(1, nrow = 3, ncol = 3)
+res1 <- dispersal(
+    dispersal_kernel = kernel,
+    abundance = abu
+)
+expect_equal(
+    sum(abu),
+    sum(res1),
+    tolerance = 0.001,
+    info = "dispersal doesn't loose idividuals."
+)
+expect_true(
+    {
+        m <- n / 2
+        sd(res1[(m - 1) :(m + 1), (m - 1) : (m + 1)]) < 0.001
+    },
+    info = "kernel is correctly applied."
+)
+
+abu <- matrix(0, nrow = n, ncol = n)
+abu[n / 2, 1] <- 6
+abu[n / 2, 2:(n-1)] <- 9
+abu[n / 2, n] <- 6
+kernel <- matrix(1, nrow = 3, ncol = 3)
+res1 <- dispersal(
+    dispersal_kernel = kernel,
+    abundance = abu
+)
+expect_true(
+    {
+        all(
+            c(res1[n / 2, c(1,n)] == 2,
+            res1[n / 2, 2:(n-1)] == 3)
+        )
+    },
+    info = "edge effects are only because of missing influce cells beyond the edge."
+)
 rm(n, n2, abu, suitab, kernel, res1, res2)
